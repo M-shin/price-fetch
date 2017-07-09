eb4c4b7866f13ce4375171a7164c5199e1ec82bc
mshin <mshin@godaddy.com>
2017-07-09 16:57:27 -0700

    Initial config file

---
+++ config/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  exs: {
+    gdax: {
+      name: 'GDAX',
+      url: 'https://api.gdax.com'
+    },
+    kraken: {
+      name: 'Kraken',
+      url: 'https://api.kraken.com'
+    },
+    poloniex: {
+      name: 'Poloniex',
+      url: 'https://poloniex.com'
+    }
+  },
+  interval: 1000
+};
